189467fa8a0feac9ebf620d01d780de3e66c4888
Ferdi Akdogan <ferdiakdogan@outlook.com>
2019-07-14 18:24:51 +0300

    Client class is implemented and Art class updated.

--- veneer.py
+++ veneer.py
@@ -1,13 +1,18 @@
 class Art:
-    def __init__(self, artist, title, year, medium):
+    def __init__(self, artist, title, year, medium, owner):
         self.artist = artist
         self.title = title
         self.medium = medium
         self.year = year
+        self.owner = owner
 
     def __repr__(self):
-        return "{artist}. \"{title}\". {year}, {medium}.".format(artist=self.artist, title=self.title, year=self.year,
-                                                                 medium=self.medium)
+        return "{artist}. \"{title}\". {year}, {medium}, {ownername}, {ownerlocation}.".format(artist=self.artist,
+                                                                                               title=self.title,
+                                                                                               year=self.year,
+                                                                                               medium=self.medium,
+                                                                                               ownername=self.owner.name,
+                                                                                               ownerlocation=self.owner.location)
 
 
 class Marketplace:
@@ -25,13 +30,24 @@ class Marketplace:
             print(listing)
 
 
-girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", 1910, "oil in canvas")
+class Client:
+    def __init__(self, name, is_museum, location="Private Collection"):
+        self.name = name
+        self.location = location
+        self.is_museum = is_museum
 
-# print(girl_with_mandolin)
 
 veneer = Marketplace()
 
-print(veneer.show_listings())
+# print(veneer.show_listings())
+
+edytta = Client("Edytta Halpirt", False)
+moma = Client("The MOMA", "New York", True)
+
+girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", 1910, "oil in canvas", edytta)
+
+print(girl_with_mandolin)
+
 
 
 
